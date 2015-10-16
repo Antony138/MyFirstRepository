460c5711b5b3bba8674bbf59726de9cf69d8d451
Antony.Wong <aeq2005@163.com>
2015-10-16 16:24:08 +0800

    Creat a new project(base Swift)

---
+++ Project/MyFirstRepository/MyFirstRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstRepository
+//
+//  Created by YuHeng_Antony on 10/16/15.
+//  Copyright © 2015 Homni Electron Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
