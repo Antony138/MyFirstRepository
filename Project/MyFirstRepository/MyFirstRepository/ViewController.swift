0ebace9594c67a2b93b3b04cbd3eb982ccc3784c
Antony.Wong <aeq2005@163.com>
2015-10-16 16:57:39 +0800

    NewBranch modify.

--- Project/MyFirstRepository/MyFirstRepository/ViewController.swift
+++ Project/MyFirstRepository/MyFirstRepository/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// This is a new Branch
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
